744961bb7a475260e5a834a42e370af5f2eb6d0b
scopophobic <66241061+scopophobic@users.noreply.github.com>
2022-05-01 12:55:29 +0530

    Create Problem Code:GROUPASSGN.cpp

---
+++ codechef practice/Problem Code:GROUPASSGN.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    int a=n*2;
+	    int ans;
+	    ans=a+1-x;
+	    cout<<ans<<endl;
+	}
+	return 0;
+}
