d81faad5d5545e7316e9dabbf9496b49aa9e7037
scopophobic <66241061+scopophobic@users.noreply.github.com>
2022-05-01 19:17:12 +0530

    Create Backspace String Compare.cpp

---
+++ leetcode questions/Backspace String Compare.cpp
@@ -0,0 +1,35 @@
+
+//https://leetcode.com/problems/backspace-string-compare/
+
+class Solution {
+public:
+    bool backspaceCompare(string S, string T) {
+        stack<char> s, t;
+        
+        for(char ch : S)
+        {
+            if(ch == '#'){
+                if(s.size()) s.pop();
+            }
+            else s.push(ch);
+        }
+        
+        for(char ch : T)
+        {
+            if(ch == '#'){
+                if(t.size()) t.pop();
+            }
+            
+            else t.push(ch);
+        }
+        
+        while(s.size() && t.size())
+        {
+            if(s.top() != t.top()) return false;
+            s.pop(), t.pop();
+        }
+        
+        if(s.size() || t.size()) return false;
+        return true;
+    }
+};
